52a7f30bbfe0cc0f32f4dc6d248efee9f0cf345c
JHM Darbyshire (MBP) <attack68@users.noreply.github.com>
2022-05-23 11:25:23 +0100

    PCA: add risk methods and example Notebook

--- modules/pca.py
+++ modules/pca.py
@@ -11,3 +11,15 @@ class PCA_:
         lambd, E = self.pca(Q)
         centralised_data = data - data.mean(axis=0)
         return np.matmul(centralised_data, E)
+
+    def pca_risk(self, curve, Q):
+        S = self.risk(curve)
+        lambd, E = self.pca(Q)
+        return np.matmul(E.T, S)
+
+    def pca_covar_alloc(self, curve, Q):
+        S_tilde = self.pca_risk(curve, Q)
+        lambd, E = self.pca(Q)
+        c = self.covar(curve, Q)
+        return (S_tilde[:, 0]**2 * lambd / c)[:, np.newaxis]
+
