5951646a4a9f0cff5e1f94847210089eeb468ccd
JHM Darbyshire (MBP) <attack68@users.noreply.github.com>
2022-05-24 11:26:40 +0100

    PCA: add trade adjustment code and extend Notebook

--- modules/pca.py
+++ modules/pca.py
@@ -23,3 +23,22 @@ class PCA_:
         c = self.covar(curve, Q)
         return (S_tilde[:, 0]**2 * lambd / c)[:, np.newaxis]
 
+    def pca_hedge_adjustment(self, Q, S_ini, H=[0], L=None):
+        """defaults to hedging directionality: PC1 is set to zero"""
+        lambd, E = self.pca(Q)
+        n, n2 = len(lambd), len(H)
+        E_H = E[:, H]
+        if L is not None:
+            n3 = len(L)
+            L_ = np.zeros(shape=(n3, n))
+            for row, col in enumerate(L):
+                L_[row, col] = 1.0
+            A = np.block([[np.eye(n), E_H, L_.T],
+                          [E_H.T, np.zeros((n2, n2)), np.zeros((n2, n3))],
+                          [L_, np.zeros((n3, n2)), np.zeros((n3, n3))]])
+            b = np.block([[np.zeros((n, 1))],
+                         [-np.matmul(E_H.T, S_ini)],
+                         [np.zeros((n3, 1))]])
+            return np.linalg.solve(A, b)[:n, :]
+        else:
+            return -np.matmul(E_H, np.matmul(E_H.T, S_ini))
